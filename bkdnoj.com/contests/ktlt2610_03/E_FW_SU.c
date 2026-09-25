8e7f285d8a0779bf374dd3a909c8394d38d238b2
TPSquare <t2p0t0p8@gmail.com>
2026-09-25 12:07:35 +0700

    ktlt2610_03

--- bkdnoj.com/contests/ktlt2610_03/E_FW_SU.c
+++ bkdnoj.com/contests/ktlt2610_03/E_FW_SU.c
@@ -3,7 +3,21 @@
 int main() {
   long n, i, res = 0;
   scanf("%ld", &n);
-  for (i = 1; i <= n; i++)
-    if (n % i == 0) ++res;
+  for (i = 1; i * i <= n; i++)
+    if (n % i == 0) res += i != (n / i) ? 2 : 1;
   printf("%ld", res);
 }
+
+// int main() {
+//   long n, kq = 0, i;
+//   scanf("%ld", &n);
+//   for (i = 1; i * i <= n; i++) {
+//     if (n % i == 0) {
+//       if (n / i == i)
+//         kq += 1;
+//       else
+//         kq += 2;
+//     }
+//   }
+//   printf("%ld", kq);
+// }
--- bkdnoj.com/contests/ktlt2610_03/F_FW_NP.c
+++ bkdnoj.com/contests/ktlt2610_03/F_FW_NP.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  long n, du, sl0 = 0;
+  unsigned long long res = 0, k = 1;
+  long n;
   scanf("%ld", &n);
   if (n == 0) {
     printf("0");
     return 0;
   }
-  unsigned long long res = 0;
-  while (1) {
-    du = n % 2;
-    if (du == 1) break;
-    ++sl0;
+  while (n) {
+    res = k * (n % 2) + res;
     n /= 2;
+    k *= 10;
   }
-  while (n != 0) {
-    res = res * 10 + n % 2;
-    n /= 2;
-  }
-  while (res != 0) {
-    printf("%llu", res % 10);
-    res /= 10;
-  }
-  while (sl0--) printf("0");
+  printf("%llu", res);
 }
